eb8e3fcef6e6416f630265a0da138a7eaa5a674d
yigongliu.go <yigongliu.go@e200470a-fbef-11de-a1eb-ffa25d41dba3>
2013-09-24 21:35:12 +0000

    tag old code; check existing code work with go1.1

---
+++ tags/tag-9-24-2013/README.txt
@@ -0,0 +1,70 @@
+There are two implementations of "router" package; both provide the same user API.
+
+branches/router1:
+. strictly follow Go's idea of "Do not communicate by sharing memory; 
+           instead, share memory by communicating."
+. avoid directly sharing memory among goroutines (thus avoid locks/mutex); 
+  each data has a owner and can only be changed by owner; 
+  others have to talk to owners thru channels to access the data
+
+"ping-pong" tests:
+(bouncing 300K msgs between "Pinger" goroutine and "Ponger" goroutine)
+test setup:
+      machine: Pentium(R) Dual CPU E2160 @ 1.80GHz with 2G RAM
+      GOMAXPROCS=32
+
+. pingpong1: using direct channels between Pinger and Ponger:
+real	0m1.763s
+user	0m0.540s
+sys	0m1.220s
+
+. pingpong2: Pinger/Ponger using channels connected thru one router:
+real	0m8.851s
+user	0m3.868s
+sys	0m4.972s
+
+. pingpong3: Pinger's channels connected to router1 and Ponger's channels connected to
+  router2 and router1/router2 are directly connected:
+real	0m21.079s
+user	0m9.765s
+sys	0m11.169s
+
+. pingpong4: Pinger's channels connected to router1 and Ponger's channels connected to
+  router2 and router1/router2 are connected thru unix socket:
+real	0m49.062s
+user	0m28.570s
+sys	0m21.261s
+
+
+trunk (router2):
+. directly share memory among various goroutines for better efficiency, shared
+  memory are protected by sync.Mutex
+. router's routing-table, endpoint's binding_set, and proxy's RecvChanBundle and
+  SendChanBundle are directly shared and locked.
+. trunk (router2) is a bit more efficient than branches/router1.
+
+"ping-pong" tests: 
+same setup as above
+
+. pingpong1: using direct channels between Pinger and Ponger:
+real	0m1.769s
+user	0m0.584s
+sys	0m1.204s
+
+. pingpong2: Pinger/Ponger using channels connected thru one router:
+real	0m4.243s
+user	0m1.660s
+sys	0m2.580s
+
+. pingpong3: Pinger's channels connected to router1 and Ponger's channels connected to
+  router2 and router1/router2 are directly connected:
+real	0m6.622s
+user	0m2.704s
+sys	0m3.876s
+
+. pingpong4: Pinger's channels connected to router1 and Ponger's channels connected to
+  router2 and router1/router2 are connected thru unix socket:
+real	0m34.004s
+user	0m20.505s
+sys	0m14.929s
+
---
+++ tags/tag-9-24-2013/routedchan.go
@@ -0,0 +1,284 @@
+//
+// Copyright (c) 2010 - 2011 Yigong Liu
+//
+// Distributed under New BSD License
+//
+
+package router
+
+import (
+	"sync"
+)
+
+type ChanDirection int
+
+const (
+	Send ChanDirection = iota
+	Recv
+)
+
+type operType int
+
+const (
+	attachOp operType = iota
+	detachOp
+)
+
+type oper struct {
+	kind operType
+	peer *RoutedChan
+}
+
+/*
+ RoutedChan represents channels which are attached to router. 
+ They expose Channel's interface: Send()/TrySend()/Recv()/TryRecv()/...
+ and additional info:
+    1. Id - the id which channel is attached to
+    2. NumPeers() - return the number of bound peers
+    3. Peers() - array of bound peers RoutedChan
+    4. Detach() - detach the channel from router
+*/
+type RoutedChan struct {
+	Kind         ChanDirection
+	Id           Id
+	Channel      //external SendChan/RecvChan, attached by clients
+	router       *routerImpl
+	dispatcher   Dispatcher //current for push dispacher, only sender uses dispatcher
+	bindChan     chan *BindEvent
+	bindCond     *sync.Cond
+	bindLock     sync.Mutex
+	bindings     []*RoutedChan //binding_set
+	inDisp       bool          //in a dispatch loop
+	opBuf        []*oper
+	internalChan bool
+	detached     bool
+	delayClose   func(*RoutedChan)
+}
+
+func newRoutedChan(id Id, t ChanDirection, ch Channel, r *routerImpl, bc chan *BindEvent) *RoutedChan {
+	routCh := &RoutedChan{}
+	routCh.Kind = t
+	routCh.Id = id
+	routCh.Channel = ch
+	routCh.router = r
+	routCh.bindChan = bc
+	if t == Send {
+		routCh.bindCond = sync.NewCond(&routCh.bindLock)
+	}
+	return routCh
+}
+
+func (e *RoutedChan) Interface() interface{} {
+	return e
+}
+
+//override Channel.Close() method
+func (e *RoutedChan) Close() {
+	if e.Kind == Send {
+		//recover panic to handle race(close twice) when proxy destroy and a sender chan close from outside of router at the same time
+		defer func() {
+			_ = recover()
+		}()
+		//wake up sender goroutine blocked waiting for peers
+		e.bindLock.Lock()
+		e.detached = true
+		e.bindCond.Broadcast()
+		e.bindLock.Unlock()
+	}
+	//close sender chan so sender goroutine will exit
+	e.Channel.Close()
+}
+
+func (e *RoutedChan) NumPeers() int {
+	e.bindLock.Lock()
+	defer e.bindLock.Unlock()
+	num := len(e.bindings)
+	for i := 0; i < len(e.opBuf); i++ {
+		op := e.opBuf[i]
+		switch op.kind {
+		case attachOp:
+			num++
+		case detachOp:
+			num--
+		}
+	}
+	return num
+}
+
+func (e *RoutedChan) Peers() (copySet []*RoutedChan) {
+	e.bindLock.Lock()
+	defer e.bindLock.Unlock()
+	l := len(e.bindings)
+	if l == 0 {
+		return
+	}
+	copySet = make([]*RoutedChan, l)
+	copy(copySet, e.bindings)
+	return
+}
+
+func (e *RoutedChan) start(disp DispatchPolicy) {
+	if e.Kind == Send {
+		e.dispatcher = disp.NewDispatcher()
+		go e.senderLoop()
+	}
+}
+
+func (e *RoutedChan) senderLoop() {
+	cont := true
+	for cont {
+		e.bindLock.Lock()
+		//block here till we have recvers so that message will not be lost
+		for len(e.bindings) == 0 && !e.detached {
+			e.bindCond.Wait()
+		}
+		e.bindLock.Unlock()
+		v, chOpen := e.Channel.Recv()
+		if chOpen {
+			e.bindLock.Lock()
+			if len(e.bindings) > 0 {
+				e.inDisp = true
+			}
+			e.bindLock.Unlock()
+			if e.inDisp {
+				e.dispatcher.Dispatch(v, e.bindings)
+			}
+			e.bindLock.Lock()
+			e.inDisp = false
+			if len(e.opBuf) > 0 {
+				e.runPendingOps()
+			}
+			e.bindLock.Unlock()
+		} else {
+			e.router.detach(e, true)
+			cont = false
+		}
+	}
+}
+
+func (e *RoutedChan) runPendingOps() {
+	for i := 0; i < len(e.opBuf); i++ {
+		op := e.opBuf[i]
+		switch op.kind {
+		case attachOp:
+			e.attachImpl(op.peer)
+			if e.Kind == Send {
+				op.peer.attach(e)
+			}
+		case detachOp:
+			e.detachImpl(op.peer)
+			if e.Kind == Send {
+				op.peer.detach(e)
+			}
+		}
+		e.opBuf[i] = nil
+	}
+	//clean up
+	e.opBuf = e.opBuf[0:0]
+}
+
+func (e *RoutedChan) Detach() {
+	e.router.detach(e, false)
+}
+
+func (e *RoutedChan) attachImpl(p *RoutedChan) {
+	e.bindings = append(e.bindings, p)
+	if e.bindChan != nil {
+		//KeepLatest non-blocking send
+	L:
+		for {
+			select {
+			case e.bindChan <- &BindEvent{PeerAttach, len(e.bindings)}:
+				break L
+			default:
+				<-e.bindChan //drop the oldest one
+			}
+		}
+	}
+	if e.Kind == Send && len(e.bindings) == 1 { //first recver attached
+		e.bindCond.Broadcast()
+	}
+}
+
+func (e *RoutedChan) attach(p *RoutedChan) {
+	e.bindLock.Lock()
+	defer e.bindLock.Unlock()
+	if e.inDisp {
+		e.opBuf = append(e.opBuf, &oper{attachOp, p})
+	} else {
+		e.attachImpl(p)
+		if e.Kind == Send {
+			p.attach(e)
+		}
+	}
+}
+
+func (e *RoutedChan) detachImpl(p *RoutedChan) {
+	n := len(e.bindings)
+	for i, v := range e.bindings {
+		if v == p {
+			e.bindings[i] = nil
+			e.bindings = append(e.bindings[:i], e.bindings[i+1:]...)
+			if e.bindChan != nil {
+				//KeepLatest non-blocking send
+			L:
+				for {
+					select {
+					case e.bindChan <- &BindEvent{PeerDetach, n - 1}: //chan full
+						break L
+					default:
+						<-e.bindChan //drop the oldest one
+					}
+				}
+			}
+			if len(e.bindings) == 0 {
+				switch e.Kind {
+				case Recv:
+					//for recver, if all senders Detached
+					//send EndOfData to notify possible pending goroutine
+					if e.bindChan != nil {
+						//if bindChan exist, user is monitoring bind status
+						//send EndOfData event and normally leave ext chan "ch" open
+					L1:
+						for {
+							select {
+							case e.bindChan <- &BindEvent{EndOfData, 0}:
+								break L1
+							default:
+								<-e.bindChan
+							}
+						}
+						if e.detached {
+							e.Close()
+						}
+					} else {
+						//since no bindChan, user code is not monitoring bind status
+						//close ext chan to notify potential pending goroutine
+						detached := e.detached
+						e.bindLock.Unlock() //
+						e.Close()
+						if !detached {
+							e.Detach() //remove self from routing table
+						}
+						e.bindLock.Lock()
+						return
+					}
+				}
+			}
+			return
+		}
+	}
+}
+
+func (e *RoutedChan) detach(p *RoutedChan) {
+	e.bindLock.Lock()
+	defer e.bindLock.Unlock()
+	if e.inDisp {
+		e.opBuf = append(e.opBuf, &oper{detachOp, p})
+	} else {
+		e.detachImpl(p)
+		if e.Kind == Send {
+			p.detach(e)
+		}
+	}
+}
